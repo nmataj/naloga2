85ed92e73ed4da1fb56d48ef73e249628878c4a2
Nuša Mataj <nusa.mataj@student.um.si>
2022-11-04 14:15:43 +0100

    Fix

--- weather-forecast/src/app/app-routing.module.ts
+++ weather-forecast/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherForecastComponent } from './components/weather-forecast/weather-forecast.component';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WeatherForecastComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- weather-forecast/src/app/app.module.ts
+++ weather-forecast/src/app/app.module.ts
@@ -4,14 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { WeatherForecastComponent } from './components/weather-forecast/weather-forecast.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardHeaderComponent } from './components/card-header/card-header.component';
+import { CurrentWeatherComponent } from './components/current-weather/current-weather.component';
+import { WeatherFiveDaysComponent } from './components/weather-five-days/weather-five-days.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherForecastComponent,
+    HeaderComponent,
+    CardHeaderComponent,
+    CurrentWeatherComponent,
+    WeatherFiveDaysComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
+  exports: [ HeaderComponent ],
   providers: [],
   bootstrap: [AppComponent]
 })
